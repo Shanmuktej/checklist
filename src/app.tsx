285cb1b3aac1ed4c6d49f4bc8526263ac2097c78
Shanmuktej <shanmukaanem@gmail.com>
2023-11-23 18:49:09 +0530

    added . in src of images

--- src/app.tsx
+++ src/app.tsx
@@ -12,7 +12,7 @@ export function App() {
         style=" margin: 24px"
       >
         <DotLottiePlayer
-          src="/assets/check.lottie"
+          src="./assets/check.lottie"
           className="h-11 w-11"
           autoplay
           loop
